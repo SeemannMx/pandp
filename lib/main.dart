5497416f7ec02efe562234aa535815450b764383
Tino Kallinich <tkallinich-friedrich@inovex.de>
2021-01-20 15:31:29 +0100

    added hover dragable line

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,7 @@ class PandP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // dev.log('enter', name: id);
-
-    return MaterialApp(
-      title: 'PandP',
-      home: HomePage()
-    );
+    return MaterialApp(title: 'PandP', home: HomePage());
   }
 }
 
@@ -30,16 +26,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var flexLeft = 300;
   var flexRight = 300;
+  var sliderWidth = 3.0;
 
   Size size;
+
+  bool dragColorSwitch = true;
+  Color dragContainerColor = Colors.blueGrey;
+
+  Widget dragItem = Icon(Icons.switch_left);
 
   @override
   Widget build(BuildContext context) {
     // dev.log('enter', name: HomePage.route);
     size = MediaQuery.of(context).size;
+    sliderWidth = size.width * 0.0025;
 
     return Container(
-      color: Colors.black45,
+      color: Colors.blueGrey,
       height: size.height,
       width: size.width,
       child: Row(
@@ -47,22 +50,20 @@ class _HomePageState extends State<HomePage> {
           Flexible(
             flex: flexLeft,
             child: Container(
-              color: Colors.blue,
+              color: Colors.black87,
             ),
           ),
           GestureDetector(
-            onHorizontalDragStart: (DragStartDetails start) => _start(context, start),
-            onHorizontalDragUpdate: (DragUpdateDetails update) => _update(context, update),
-            // onHorizontalDragEnd: _end(),
-            child: Container(
-              width: 100,
-              color: Colors.red,
-            ),
-          ),
+              onHorizontalDragUpdate: (DragUpdateDetails update) =>
+                  _update(context, update),
+              child: MouseRegion(
+                child: Container(width: sliderWidth, color: dragContainerColor),
+                onHover: (e) => _hover(),
+              )),
           Flexible(
             flex: flexRight,
             child: Container(
-              color: Colors.green,
+              color: Colors.black87,
             ),
           ),
         ],
@@ -70,33 +71,29 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _start(BuildContext context, DragStartDetails start){
-    // dev.log('start', name: HomePage.route);
+  _hover() {
+    dev.log('hover', name: HomePage.route);
 
-    RenderBox getBox = context.findRenderObject();
-    var local = getBox.globalToLocal(start.globalPosition);
-
-    // dev.log('start ${local.dx} , ${local.dy}', name: HomePage.route);
+    setState(() {
+      if (dragColorSwitch) {
+        dragContainerColor = Colors.pink;
+      } else {
+        dragContainerColor = Colors.blueGrey;
+      }
+      dragColorSwitch = !dragColorSwitch;
+    });
   }
 
-  _end(BuildContext context){
-    dev.log('end', name: HomePage.route);
-  }
-
-  _update(BuildContext context, DragUpdateDetails update){
-
+  _update(BuildContext context, DragUpdateDetails update) {
     RenderBox getBox = context.findRenderObject();
     var local = getBox.globalToLocal(update.globalPosition);
 
     setState(() {
       var max = size.width;
-      // dev.log('update left : $max - ${local.dx} = $flexLeft ', name: HomePage.route);
-      // dev.log('update right: $max - $flexLeft   = $flexRight ', name: HomePage.route);
-
       flexRight = (max - local.dx).round();
-       flexLeft = (max - flexRight).round();
+      flexLeft = (max - flexRight).round();
     });
 
-    dev.log('update ${local.dx}', name: HomePage.route);
+    // dev.log('${local.dx} ${local.dy}', name: HomePage.route);
   }
 }
