bbfea1f52f1581cd4187a1c91f265b0a77631b03
Tino Kallinich <tkallinich-friedrich@inovex.de>
2021-01-21 19:05:17 +0100

    responsive grid

--- lib/provider/provider_image.dart
+++ lib/provider/provider_image.dart
@@ -3,23 +3,44 @@ import 'package:flutter/material.dart';
 import 'dart:developer' as dev;
 
 class CustomImageProvider extends ChangeNotifier {
-
   var images = [];
   var locations = [];
+  var widget;
 
-  CustomImageProvider() {_load();}
+  CustomImageProvider() {
+    _load();
+  }
 
   _load() {
-    images.add(Image(image: AssetImage('assets/img/chart.png')));
-
-    locations.add(Image(image: AssetImage('assets/img/wappen_siedlung.png')));
-    locations.add(Image(image: AssetImage('assets/img/wappen_neu_anfang.png')));
-    locations.add(Image(image: AssetImage('assets/img/wappen_neu_farmland.png')));
-    locations.add(Image(image: AssetImage('assets/img/wappen_nord_wall.png')));
-    locations.add(Image(image: AssetImage('assets/img/wappen_rhin_hafen.png')));
-    locations.add(Image(image: AssetImage('assets/img/wappen_spezial_einheit.png')));
-    locations.add(Image(image: AssetImage('assets/img/wappen_west_defense.png')));
+    _addChart();
+    _addLocations();
 
     notifyListeners();
+  }
+
+  _addChart() {
+    images.add(Image(image: AssetImage('assets/img/chart.png')));
+  }
+
+  _addLocations() {
+    int value = 25;
+
+    locations
+      ..add(_getWappen(value ,Image(image: AssetImage('assets/img/wappen_siedlung.png'))))
+      ..add(_getWappen(value ,Image(image: AssetImage('assets/img/wappen_neu_anfang.png'))))
+      ..add(_getWappen(value ,Image(image: AssetImage('assets/img/wappen_neu_farmland.png'))))
+      ..add(_getWappen(value ,Image(image: AssetImage('assets/img/wappen_nord_wall.png'))))
+      ..add(_getWappen(value ,Image(image: AssetImage('assets/img/wappen_rhin_hafen.png'))))
+      ..add(_getWappen(value ,Image(image: AssetImage('assets/img/wappen_spezial_einheit.png'))))
+      ..add(_getWappen(value ,Image(image: AssetImage('assets/img/wappen_west_defense.png'))));
+  }
+
+  _getWappen(int flex, Widget child){
+    return Flexible(
+        flex: flex,
+        fit: FlexFit.tight,
+        child: Container(
+            child: child
+        ));
   }
 }
--- lib/widgets/location_grid.dart
+++ lib/widgets/location_grid.dart
@@ -7,100 +7,51 @@ class LocationsGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<CustomImageProvider>(
         builder: (context, imageProvider, child) {
-          int flex = 20;
-          return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Flexible(flex: 5, child: Container()),
-            Flexible(
-              flex: flex,
-                fit: FlexFit.tight,
-              child: Container(
-                // padding: EdgeInsets.all(25),
-                child: imageProvider.locations[0],
-              )
-            ),
-
-            Flexible(flex: 5, child: Container()),
-
-            Flexible(
-              flex: flex,
-              child: Column(
-                children: [
-                  Flexible(flex: 10, child: Container()),
-                  Flexible(
-                      flex: 50,
-                      fit: FlexFit.tight,
-                      child: Container(
-                        child: imageProvider.locations[1],
-                      )),
-                  Flexible(flex: 10, child: Container()),
-                  Flexible(
-                      flex: 50,
-                      fit: FlexFit.tight,
-                      child: Container(
-                        // padding: EdgeInsets.all(25),
-                        child: imageProvider.locations[2],
-                      )),
-                  Flexible(flex: 10, child: Container()),
-                ],
-              ),
-            ),
-            Flexible(
-                flex: 5,
-                child: Container()
-            ),
-            Flexible(
-              flex: flex,
-              child: Column(
-                children: [
-                  Flexible(flex: 10, child: Container()),
-                  Flexible(
-                      flex: 50,
-                      fit: FlexFit.tight,
-                      child: Container(
-                        // padding: EdgeInsets.all(25),
-                        child: imageProvider.locations[3],
-                      )),
-                  Flexible(flex: 10, child: Container()),
-                  Flexible(
-                      flex: 50,
-                      fit: FlexFit.tight,
-                      child: Container(
-                        // padding: EdgeInsets.all(25),
-                        child: imageProvider.locations[4],
-                      )),
-                  Flexible(flex: 10, child: Container()),
-                ],
-              ),
-            ),
-            Flexible(
-                flex: 5,
-                child: Container()
-            ),
-            Flexible(
-              flex: flex,
-              child: Column(
-                children: [
-                  Flexible(flex: 10, child: Container()),
-                  Flexible(
-                      flex: 50,
-                      fit: FlexFit.tight,
-                      child: Container(
-                        // padding: EdgeInsets.all(25),
-                        child: imageProvider.locations[5],
-                      )),
-                  Flexible(flex: 10, child: Container()),
-                  Flexible(
-                      flex: 50,
-                      fit: FlexFit.tight,
-                      child: Container(
-                        // padding: EdgeInsets.all(25),
-                        child: imageProvider.locations[6],
-                      )),
-                  Flexible(flex: 10, child: Container()),
-                ],
-              ),
-            )
-          ]);
-        });
+      int flex = 20;
+      return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        Flexible(flex: 5, child: Container()),
+        imageProvider.locations[0],
+        Flexible(flex: 5, child: Container()),
+        Flexible(
+          flex: flex,
+          child: Column(
+            children: [
+              Flexible(flex: 10, child: Container()),
+              imageProvider.locations[1],
+              Flexible(flex: 10, child: Container()),
+              imageProvider.locations[2],
+              Flexible(flex: 10, child: Container()),
+            ],
+          ),
+        ),
+        Flexible(flex: 5, child: Container()),
+        Flexible(
+          flex: flex,
+          child: Column(
+            children: [
+              Flexible(flex: 10, child: Container()),
+              imageProvider.locations[3],
+              Flexible(flex: 10, child: Container()),
+              imageProvider.locations[4],
+              Flexible(flex: 10, child: Container()),
+            ],
+          ),
+        ),
+        Flexible(flex: 5, child: Container()),
+        Flexible(
+          flex: flex,
+          child: Column(
+            children: [
+              Flexible(flex: 10, child: Container()),
+              imageProvider.locations[5],
+              Flexible(flex: 10, child: Container()),
+              imageProvider.locations[6],
+              Flexible(flex: 10, child: Container()),
+            ],
+          ),
+        ),
+        Flexible(flex: 5, child: Container()),
+      ]);
+    });
   }
 }
